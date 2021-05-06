e172722000131e49b93ebed285e605ffa0123744
Nicholas Synovic <nicholas.synovic@gmail.com>
2021-05-06 17:40:39 -0500

    Add command line arguement parser

---
+++ libs/cmdLineArgs.py
@@ -0,0 +1,57 @@
+import argparse
+from argparse import Namespace
+
+
+def arguementHandler() -> Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Game Jame Story Gen Scraper",
+        usage="A tool to scrape writing prompt forumns for ideas and to store them in a database.",
+        epilog="Created by Nicholas M. Synovic.",
+    )
+
+    parser.add_argument(
+        "-u",
+        "--username",
+        nargs=1,
+        type=str,
+        required=True,
+        help="Your Reddit account's username",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--password",
+        nargs=1,
+        type=str,
+        required=True,
+        help="Your Reddit account's password",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--client-id",
+        nargs=1,
+        type=str,
+        required=True,
+        help="""The Client ID for this application.
+            Can be generated at https://www.reddit.com/prefs/apps""",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--client-secret",
+        nargs=1,
+        type=str,
+        required=True,
+        help="""The Client Secret for this application.
+            Can be generated at https://www.reddit.com/prefs/apps""",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    print(
+        """This file is not meant to be ran as a standalone program.
+    Please import this file into your application"""
+    )
---
+++ login.py
@@ -0,0 +1,6 @@
+import requests
+
+class Login:
+
+    def __init__(self)  ->  None:
+        self.tokenURL = "https://www.reddit.com/api/v1/access_token"
