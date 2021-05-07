c7ff5d00f3c3af0770194a3f3a2e1e0d2cede09f
Nicholas Synovic <26028870+NicholasSynovic@users.noreply.github.com>
2021-05-06 22:56:28 -0500

    Removed unneeded command line arguements

--- libs/cmdLineArgs.py
+++ libs/cmdLineArgs.py
@@ -4,27 +4,9 @@ from argparse import Namespace
 
 def argumentHandler() -> Namespace:
     parser = argparse.ArgumentParser(
-        prog="Game Jame Story Gen Scraper",
+        prog="Upgraded-Funicular",
         usage="A tool to scrape writing prompt forumns for ideas and to store them in a database.",
         epilog="Created by Nicholas M. Synovic.",
-    )
-
-    parser.add_argument(
-        "-u",
-        "--username",
-        nargs=1,
-        type=str,
-        required=True,
-        help="Your Reddit account's username",
-    )
-
-    parser.add_argument(
-        "-p",
-        "--password",
-        nargs=1,
-        type=str,
-        required=True,
-        help="Your Reddit account's password",
     )
 
     parser.add_argument(
@@ -54,8 +36,7 @@ def argumentHandler() -> Namespace:
         type=str,
         required=False,
         default=None,
-        help="""If specified, the content of this file is compared against the
-            new scraped content.
+        help="""If specified, the content of this file is compared against the new scraped content.
             Must end in a .csv format""",
     )
 
