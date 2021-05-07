2edc6e023ade08c769dcb64ce50710760cd946ec
Nicholas Synovic <nicholas.synovic@gmail.com>
2021-05-06 20:41:16 -0500

    Remove login lib

--- scraper.py
+++ scraper.py
@@ -1,40 +1,48 @@
-import praw
 import pandas
+import praw
 from pandas import DataFrame
+from progress.spinner import MoonSpinner
+
 from libs.cmdLineArgs import argumentHandler
+from libs.login import Login
+
 
 class Scraper:
-
-    def __init__(self)  ->  None:
+    def __init__(self) -> None:
         args = argumentHandler()
-        
+
         self.subredditSet: list = ["WritingPrompts", "DirtyWritingPrompts"]
         self.reddit = praw.Reddit(
-                client_id=args.client_id[0],
-                client_secret=args.client_secret[0],
-                user_agent="GameJamStoryGen/0.1 by {}".format(args.username[0])
-            )
+            client_id=args.client_id[0],
+            client_secret=args.client_secret[0],
+            user_agent="GameJamStoryGen/0.1 by {}".format(args.username[0]),
+        )
 
-    def scrapeData(self)    ->  DataFrame:
-        columnNames:list = ["Subreddit", "NSFW", "Author", "Title", "URL"]
+    def scrapeData(self) -> DataFrame:
+        columnNames: list = ["Name", "Subreddit", "NSFW", "Author", "Title", "URL"]
         df: DataFrame = pandas.DataFrame(columns=columnNames)
-        
+
         for subreddit in self.subredditSet:
-            for post in self.reddit.subreddit(display_name=subreddit).new(limit=None):
-                data: dict = {}
+            with MoonSpinner(message="Getting the latest 1000 posts from {}...".format(subreddit)) as spinner:
                 
-                data["Subreddit"] = subreddit
-                data["NSFW"] = post.over_18
-                data["Author"] = post.author
-                data["Title"] = post.title
-                data["URL"] = post.url
-                
-                df = df.append(data, ignore_index=True)
+                for post in self.reddit.subreddit(display_name=subreddit).new(limit=None):
+                    data: dict = {}
+
+                    data["Name"] = post.name
+                    data["Subreddit"] = subreddit
+                    data["NSFW"] = post.over_18
+                    data["Author"] = post.author
+                    data["Title"] = post.title
+                    data["URL"] = post.url
+
+                    df = df.append(data, ignore_index=True)
+                    
+                    spinner.next()
 
         df.to_csv("temp.csv")
-        return df 
+        return df
+
 
 if __name__ == "__main__":
     scraper = Scraper()
     data = scraper.scrapeData()
-
